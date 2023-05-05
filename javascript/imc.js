d8e9a18b232d5ac0967f3c81d502d626172ab8ba
João Victor <111308728+Iamjoaovictor@users.noreply.github.com>
2023-05-05 14:54:12 -0300

    Update imc.js

--- javascript/imc.js
+++ javascript/imc.js
@@ -3,7 +3,8 @@
 const nome = document.getElementById( 'nome' );
 const altura = document.getElementById( 'altura' );
 const peso = document.getElementById( 'peso' );
-const calcular = document.getElementById( 'calcular' );
+const calcular = document.getElementById( 'calc' );
+const imagem = document.getElementById( 'imagem-imc' );
 const resultado = document.getElementById( 'resultado' );
 
 
@@ -13,6 +14,7 @@ function imc () {
     const idade = document.getElementById( 'idade' ).value;
     const altura = document.getElementById( 'altura' ).value;
     const peso = document.getElementById( 'peso' ).value;
+    const imagem = document.getElementById( 'imagem-imc' );
     const resultado = document.getElementById( 'resultado' );
 
     // Verificação do Resultado do IMC
@@ -27,16 +29,40 @@ function imc () {
         if (idade >= 18 && idade <= 59) {
             if (valor_imc < 18.5) {
                 classificacao_para_adultos = 'Abaixo do peso.';
+                imagem.src = "../imagens/Baixo_Peso.jpeg"
+                imagem.style.width = '300px';
+                imagem.style.marginTop = '-400px';
+                imagem.style.height = '600px';
+
             } else if (valor_imc > 18.5 && valor_imc <= 24.9) {
                 classificacao_para_adultos = 'Com o peso ideal.';
+                imagem.src = "../imagens/Peso_Normal.jpeg"
+                imagem.style.width = '300px';
+                imagem.style.height = '650px';
+
             } else if (valor_imc > 24.9 && valor_imc <= 29.9) {
                 classificacao_para_adultos = 'Levemente acima do peso.';
+                imagem.src = "../imagens/Sobrepeso.jpeg"
+                imagem.style.width = '300px';
+                imagem.style.height = '650px';
+
             } else if (valor_imc > 30 && valor_imc <= 34.9) {
                 classificacao_para_adultos = 'Com obesidade de grau I';
+                imagem.src = "../imagens/Obesidade_Grau_I.jpeg"
+                imagem.style.width = '300px';
+                imagem.style.height = '650px';
+
             } else if (valor_imc > 34.9 && valor_imc <= 40) {
                 classificacao_para_adultos = 'Com obesidade de grau II';
+                imagem.src = "../imagens/Obesidade_Grau_II.jpeg"
+                imagem.style.width = '300px';
+                imagem.style.height = '650px';
+
             } else {
                 classificacao_para_adultos = 'Com obesidade de grau III.';
+                imagem.src = "../imagens/Obesidade_Grau_III.jpeg"
+                imagem.style.width = '300px';
+                imagem.style.height = '650px';
             }
 
         }
@@ -46,17 +72,28 @@ function imc () {
         if (idade >= 60) {
             if (valor_imc < 22) {
                 classificacao_para_idosos = 'Abaixo do peso.';
+                imagem.src = "../imagens/Baixo_Peso.jpeg"
+                imagem.style.width = '300px';
+                imagem.style.height = '650px';
+
             }else if (valor_imc > 22 && valor_imc <= 27) {
                 classificacao_para_idosos = 'Adequado ou Eurotrófico';
+                imagem.src = "../imagens/Peso_Normal.jpeg"
+                imagem.style.width = '300px';
+                imagem.style.height = '650px';
+
             }else {
                 classificacao_para_idosos = 'Sobrepeso';
+                imagem.src = "../imagens/Sobrepeso.jpeg"
+                imagem.style.width = '300px';
+                imagem.style.height = '650px';
             }
         
         }
 
         if (classificacao_para_adultos) {
             resultado.textContent = `${nome} seu imc é ${valor_imc} e você está ${classificacao_para_adultos}`;
-        } else if (classificacao_para_idosos) {
+        }else if (classificacao_para_idosos) {
             resultado.textContent = `${nome} seu imc é ${valor_imc} e você está ${classificacao_para_idosos}`;
         }
 
@@ -65,5 +102,4 @@ function imc () {
     }
 }
 
-calc.addEventListener('click', imc);
-
+calc.addEventListener ( 'click' , imc );
